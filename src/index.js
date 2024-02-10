968070d8db29f0971a8a07ddb938040a87c606b1
nikita petrov <petrov.nikita702@mail.ru>
2024-02-10 23:18:50 +0300

    Вёрстка и адартив блока Map

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import "./style.css"
 import Intro from './intro/Intro';
 import Articles from './articles/articles';
+import Map from "./map/map"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Intro />
     <Articles /> 
+    <Map />
   </React.StrictMode>
 );
 
---
+++ src/map/map.js
@@ -0,0 +1,12 @@
+function Map() {
+
+    return (<section class="map">
+        <div class="map__content">
+            <h3 class="map__title">Our hills and valleys are like nothing you’ve seen.</h3>
+            <p class="map__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet magna commodo, tempus dolor sit amet, laoreet elit. Donec mauris tortor.</p>
+            <button class="map__button">Learn More</button>
+        </div>
+    </section>);
+}
+
+export default Map;
