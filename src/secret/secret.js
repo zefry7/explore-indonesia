c7e23cc5cc5f183d1ade9b682b9c16ada3977ffa
nikita petrov <petrov.nikita702@mail.ru>
2024-03-02 21:31:20 +0300

    Адаптив блока secret

--- src/secret/secret.js
+++ src/secret/secret.js
@@ -1,4 +1,17 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules"
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
+import i1 from "./img/item1.png"
+import i2 from "./img/item2.png"
+import i3 from "./img/item3.png"
 import bg from "./img/bg.png"
+import arrowN from "./img/arrow-next.svg"
+import arrowP from "./img/arrow-prev.svg"
 
 function Secret() {
     const list = [
@@ -23,6 +36,79 @@ function Secret() {
         <div class="secret__content">
             <div class="secret__top">
                 <h2 class="secret__top-title">Explore Our Secrets</h2>
+                <div className="secret__top-container">
+                    <Swiper
+                        modules={[Pagination, Navigation, Autoplay]}
+                        slidesPerView={1}
+                        spaceBetween={0}
+                        initialSlide={1}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+                        pagination= {{
+                            el: '.secret__top-pagination',
+                            type: 'bullets',
+                        }}
+                        centeredSlides={true}
+                        breakpoints={{
+                            425: {
+                                slidesPerView: 3,
+                                spaceBetween: 31
+                            },
+                            2560: {
+                                slidesPerView: 3,
+                                spaceBetween: 42
+                            }
+
+                        }}
+                        loop={true}
+                        navigation={{
+                            nextEl: '.secret__top-button-next',
+                            prevEl: '.secret__top-button-prev',
+                        }}
+                    >
+                        <SwiperSlide>
+                            <div class="secret__top-img">
+                                <img src={i1} alt="" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div class="secret__top-img">
+                                <img src={i2} alt="" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div class="secret__top-img">
+                                <img src={i3} alt="" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div class="secret__top-img">
+                                <img src={i1} alt="" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div class="secret__top-img">
+                                <img src={i2} alt="" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div class="secret__top-img">
+                                <img src={i3} alt="" />
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
+                    <button class="secret__top-button-prev">
+                        <img src={arrowP} alt="" />
+                    </button>
+                    <button class="secret__top-button-next">
+                        <img src={arrowN} alt="" />
+                    </button>
+                    <div className="secret__top-pagination">
+
+                    </div>
+                </div>
             </div>
             <div class="secret__bottom">
                 <h3 class="secret__bottom-title">By The Numbers</h3>
@@ -40,5 +126,7 @@ function Secret() {
         </div>
     </section>
 }
+
+
 
 export default Secret;
